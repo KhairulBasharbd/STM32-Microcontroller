ecd609679d80f1ea35dd045387afdddf80b67f41
Khairul Basar <khairulbashar24853@gmail.com>
2023-08-09 13:22:23 +0600

    Led blinking with BSRR register code added

--- NUCLEO-F446RE/Led_blinking/main.c
+++ NUCLEO-F446RE/Led_blinking/main.c
@@ -10,10 +10,10 @@ int main(void){
 	
 	while(1){
 		
-		GPIOA->ODR |= 1U<<5;
-		for(int i=0;i<1000000;i++){}
+		GPIOA->BSRR |= 1U<<5;
+		for(int i=0;i<100000;i++){}
 		
-		GPIOA->ODR &= ~(1U<<5);
+		GPIOA->BSRR |= (1U<<21);
 		for(int i=0;i<100000;i++){}
 	
 	}
