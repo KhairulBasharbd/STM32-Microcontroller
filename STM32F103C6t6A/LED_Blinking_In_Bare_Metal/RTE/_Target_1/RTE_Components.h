0aa5b4d5b9be0bdb1319ada5891de09d12f09f85
Khairul Basar <khairulbashar24853@gmail.com>
2024-05-29 22:08:09 +0600

    led blinking in bare metal added

--- STM32F103C6t6A/LED_Blinking_In_Bare_Metal/RTE/_Target_1/RTE_Components.h
+++ STM32F103C6t6A/LED_Blinking_In_Bare_Metal/RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'led_blinking' 
+ * Project: 'LED_Blinking_In_Bare_Metal' 
  * Target:  'Target 1' 
  */
 
@@ -14,10 +14,8 @@
 /*
  * Define the Device Header File: 
  */
-#define CMSIS_device_header "stm32f4xx.h"
+#define CMSIS_device_header "stm32f10x.h"
 
-/* Keil::Device:Startup@2.6.3 */
-#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
 
 
 #endif /* RTE_COMPONENTS_H */
