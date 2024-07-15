3dc9accb46db9947c1d431516d30ef6c7b1044fd
Khairul Basar <khairulbashar24853@gmail.com>
2024-07-15 12:25:42 +0600

    Simple ADC added

---
+++ NUCLEO-F446RE/7. ADC Using VCC and GND/Objects/main.d
@@ -0,0 +1,11 @@
+./objects/main.o: main.c RccConfig.h \
+  D:\Installed_software\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
+  D:\Installed_software\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f446xx.h \
+  D:\Installed_software\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\core_cm4.h \
+  D:\Installed_software\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  D:\Installed_software\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_version.h \
+  D:\Installed_software\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  D:\Installed_software\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\cmsis_armclang_m.h \
+  D:\Installed_software\Keil_v5\ARM\ARMCLANG\include\arm_acle.h \
+  D:\Installed_software\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\armv7m_mpu.h \
+  D:\Installed_software\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
