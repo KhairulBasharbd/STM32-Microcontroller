9549b3164d1b17954d0f9386d91890da06ebcd2b
Khairul Basar <khairulbashar24853@gmail.com>
2024-09-26 14:06:38 +0600

    RTC With DS3231 added

---
+++ STM32F103C6t6A/I2C_with_DS3232_Seting_Getting_Time_And_Get_Temp/Objects/sysmem.d
@@ -0,0 +1,3 @@
+./objects/sysmem.o: sysmem.c \
+  D:\Installed_software\Keil_v5\ARM\ARMCLANG\include\errno.h \
+  D:\Installed_software\Keil_v5\ARM\ARMCLANG\include\stdio.h
---
+++ STM32F103C6t6A/I2C_with_DS3232_Seting_Getting_Time_And_Get_Temp/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,24 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'I2C with DS3231 and LCD' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+/* Keil::CMSIS Driver:I2C@2.2 */
+#define RTE_Drivers_I2C1                /* Driver I2C1 */
+        #define RTE_Drivers_I2C2                /* Driver I2C2 */
+
+
+#endif /* RTE_COMPONENTS_H */
